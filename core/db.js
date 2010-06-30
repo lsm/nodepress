05c8125a0550f00c6d0038ba33020cccd40a7b96
Senmiao Liu <zir.echo@gmail.com>
2010-06-30 20:54:11 +0800

    base class defined (Db, Collection)

--- core/db.js
+++ core/db.js
@@ -2,22 +2,14 @@
 var Base = require('../lib/genji/lib/genji/core/base').Base,
 Promise = require('./promise').Promise,
 mongo = require('../lib/node-mongodb-native/lib/mongodb'),
-Pool = require('./pool').Pool;
+Pool = require('./pool').Pool,
+connPool;
 
-var settings = {
-    db: {
-        host:'127.0.0.1',
-        port:27017,
-        name: 'nodepress'
-    }
-};
 var sys = require('sys');
 
 function getConn(num, callback) {
     for (var i = 0; i < num; i++) {
-        var dbServer = new mongo.Server(settings.db.host, settings.db.port, {});
-        var db = new mongo.Db(settings.db.name, dbServer, {});
-        db.open(function(err, db) {
+        mongo.connect(genji.settings.db, function(err, db) {
             if (err) throw err;
             callback(db);
         });
@@ -25,9 +17,8 @@ function getConn(num, callback) {
 }
 
 var Db = Base(function() {
-    this.pool = new Pool(getConn, 1);
+    this.pool = connPool;
 }, {
-
     giveDb: function(callback) {
         this.pool.give(callback);
     },
@@ -36,94 +27,97 @@ var Db = Base(function() {
         this.giveDb(function(db) {
             db.collection(name, function(err, coll) {
                 if (err) throw err;
-                callback(db, coll);
+                callback(coll);
             });
         });
     },
 
-    find: function(collectionName, query, options, callback) {
-        this.giveCollection(collectionName, function(db, coll) {
-            coll.find(query, options, function(err, p) {
+    _find: function(collectionName, selector, fields, options, callback) {
+        var me = this;
+        this.giveCollection(collectionName, function(coll) {
+            coll.find(selector, fields, options, function(err, cursor) {
                 if (err) throw err;
-                p.toArray(function(err, posts) {
+                cursor.toArray(function(err, result) {
                     if (err) throw err;
-                    sys.puts(posts[0].title)
-                    me.pool.emit('back', db);
+                    me.pool.emit('back', cursor.db);
+                    callback(result);
                 });
             });
         });
     },
 
-    allPosts: function() {
-        var pool = this.pool;
-        var promise = new Promise;
-        this.giveCollection('posts', function(db, cp) {
-            cp.find({}, function(err, p) {
-                if (err) return promise.reject(err);
-                p.toArray(function(err, posts) {
-                    if (err) return promise.reject(err);
-                    promise.resolve(posts);
-                    pool.emit('back', db);
-                });
-            });
-        });
-        return promise;
-    }
-});
-
-var http = require('http');
-var db = new Db();
-http.createServer(function(req, res) {
-    db.allPosts().then(function(posts) {
-        //sys.puts(posts[0].title);
-        res.writeHead(200);
-        res.end(posts[0].title);
-    }, function() {
-        sys.puts(1)
-        });
-}).listen(8000, '127.0.0.1');
-
-var Collection = Base(function(name) {
-    this.name = name;
-    this._pool = [];
-}, {
-    getDb: function() {
-        var dbServer = new mongo.Server(settings.db.host, settings.db.port, {});
-        var db = new mongo.Db(settings.db.name, dbServer, {});
-        var promise = new Promise();
-        db.open(function(err, db) {
-            if (err) throw err;
-            promise.resolve(db);
-        });
-        return promise;
-    },
-    open: function(callback) {
-        var promise = this.getDb();
+    _save: function(collectionName, data, callback) {
         var me = this;
-        promise.then(function(db) {
-            db.collection(me.name, function(err, coll) {
+        this.giveCollection(collectionName, function(coll) {
+            coll.save(data, function(err, doc) {
                 if (err) throw err;
-                callback(db, coll);
+                me.pool.emit('back', coll.db);
+                callback(doc);
             });
         });
     },
-    save: function(data, callback) {
-        this.open(function(db, coll) {
-            coll.save();
+
+    _remove: function(collectionName, selector, callback) {
+        var me = this;
+        this.giveCollection(collectionName, function(coll) {
+            coll.remove(selector, function(err, coll) {
+                if (err) throw err;
+                me.pool.emit('back', coll.db);
+                callback(coll);
+            });
         });
     },
 
-    get: function() {
+    _count: function(collectionName, selector, callback) {
         var me = this;
-        this.open(function(db, coll) {
-            coll.find({}, function(err, res) {
-                res.toArray(function(err, r) {
-                    sys.puts(sys.inspect(r));
-                    res.db.close();
-                //me._pool.push(res.db);
-                });
-                
+        this.giveCollection(collectionName, function(coll) {
+            coll.count(selector, function(err, num) {
+                if (err) throw err;
+                me.pool.emit('back', coll.db);
+                callback(num);
             });
         });
     }
 });
+
+var Collection = Db({
+    find: function(selector, fields, options) {
+        var promise = new Promise;
+        this._find(this.name, selector, fields, options, function(result) {
+            promise.resolve(result);
+        });
+        return promise;
+    },
+
+    save: function(data) {
+        var promise = new Promise;
+        this._save(this.name, data, function(doc) {
+            promise.resolve(doc);
+        })
+        return promise;
+    },
+
+    remove: function(selector) {
+        var promise = new Promise;
+        this._remove(this.name, selector, function(coll) {
+            promise.resolve(coll);
+        })
+        return promise;
+    },
+
+    count: function(selector) {
+        var promise = new Promise;
+        this._count(this.name, selector, function(num) {
+            promise.resolve(num);
+        });
+        return promise;
+    }
+});
+
+module.exports = {
+    Db: Db,
+    Collection: Collection,
+    makePool: function() {
+       if (connPool === undefined) connPool = new Pool(getConn, 5);
+    }
+}
