4368e11aeef86e2bedcab7b8f4159d22dcbe880d
Senmiao Liu <zir.echo@gmail.com>
2010-07-01 18:50:56 +0800

    account app

---
+++ app/account.js
@@ -0,0 +1,50 @@
+var db = require('../core/db'),
+Collection = db.Collection,
+view = require('../core/view'),
+settings = genji.settings,
+querystring = require("querystring"),
+auth = require('../core/auth');
+
+
+var User = Collection({
+    init: function() {
+        this._super();
+        this.name = 'users';
+    }
+});
+
+var user = new User;
+
+function signin(handler) {
+    if (auth.checkCookie(handler, settings.secureKey)) {
+        // already logged in
+        handler.send("ok");
+        return;
+    }
+    handler.on('end', function(data) {
+        var p = querystring.parse(data);
+        if (p.hasOwnProperty("username") && p.hasOwnProperty("password")) {
+            user.findOne({
+                username: p['username']
+            }).then(function(res) {
+                if (res && auth.signin(handler, p, res["password"], settings.secureKey)) {
+                    handler.send("ok");
+                } else {
+                    handler.error(401, 'Wrong username/password pair.');
+                }
+            });
+        } else {
+            handler.error(403, 'Please enter username and password.');
+        }
+    });
+}
+
+var _view = [['^/signin/$', signin, 'post']];
+
+module.exports = {
+    db: {
+        User: User,
+        user: user
+    },
+    view: _view
+}
