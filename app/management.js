dd12f0640589c8b97597b423842a667880262f2e
Senmiao Liu <zir.echo@gmail.com>
2010-07-01 22:24:49 +0800

    get/set tracker

--- app/management.js
+++ app/management.js
@@ -1,6 +1,66 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+var db = require('../core/db'),
+auth = require('../core/auth'),
+Collection = db.Collection,
+tracker;
 
 
+var Setting = Collection({
+    init: function() {
+        this._super();
+        this.name = 'settings';
+    }
+});
+
+var setting = new Setting('settings');
+
+function getTracker(handler, callback) {
+    function send(tracker) {
+        if (callback) {
+            callback(tracker);
+        } else {
+            handler.send(tracker.code);
+        }
+    }
+    if (tracker) {
+        send(tracker);
+        return;
+    }
+    setting.findOne({
+        '_id': 'defaultTracker'
+    }).then(function(trackerObj) {
+        tracker = trackerObj || {
+            code: ''
+        };
+        send(tracker);
+    });
+}
+
+function saveTracker(handler) {
+    handler.on('end', function(data) {
+        setting.save({
+            _id: 'defaultTracker',
+            code: data
+        }).then(function() {
+            tracker = {
+                code: data
+            };
+            handler.send('ok');
+        });
+    });
+}
+
+getTracker(null, function() {});
+
+var api = [
+    ['management/tracker/get/$', getTracker, 'get', [auth.checkLogin]],
+    ['management/tracker/save/$', saveTracker, 'post', [auth.checkLogin]]
+]
+
+module.exports = {
+    db: {
+        setting: setting
+    },
+    api: api,
+    getTracker: getTracker
+//    view: _view
+}
