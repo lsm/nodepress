cbecc3f90424ea0a029ee1fc3310e8f19cedad2f
Senmiao Liu <zir.echo@gmail.com>
2010-07-01 18:51:04 +0800

    in memory cache

--- core/cache.js
+++ core/cache.js
@@ -1,6 +1,25 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+var Base = genji.core.Base;
 
+exports.Cache = Base(function() {
+    this._cache = {};
+}, {
+    set: function(key, value) {
+        this._cache[key] = value;
+    },
 
+    get: function(key) {
+        return this._cache[key];
+    },
+
+    has: function(key) {
+        return this._cache.hasOwnProperty(key);
+    },
+
+    clear: function(key) {
+        if (key) {
+            delete this._cache[key];
+        } else {
+            this._cache = {};
+        }
+    }
+});
