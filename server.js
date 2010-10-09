413de3be1097c9a9aae333e8e70bb6ff4512648e
Senmiao Liu <zir.echo@gmail.com>
2010-10-09 19:11:06 +0800

    mv to bin/nodepress

--- server.js
+++
@@ -1,7 +0,0 @@
-var settings = require("./settings");
-global.genji = require('genji');
-genji.settings = settings;
-
-// start server
-var server = genji.web.startServer(settings);
-module.exports = server;
