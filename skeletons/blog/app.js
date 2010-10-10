1f0b491b03c8ba5a32fca5ac5727849ed8a9c0fc
Senmiao Liu <zir.echo@gmail.com>
2010-10-10 22:15:20 +0800

    settings.js => skeletons/blog/app.js

--- skeletons/blog/app.js
+++ skeletons/blog/app.js
@@ -1,21 +1,45 @@
 var path = require('path');
 var settings = {
-    staticUrl: 'http://127.0.0.1:8000/static/',
-    ,db: 'mongo://127.0.0.1:27017/nodepress'
-    ,cookieSecret: 'c00kie-key-4-hmac'
-    ,cookieName: '_npc'
-    ,env: 'production'
+    db: {
+        servers: 'mongo://127.0.0.1:27017/nodepress',
+        poolSize: 5
+    },
+    client: {
+        staticRoot: path.join(__dirname, '/static'),
+        staticUrl: 'http://127.0.0.1:8000/static/',
+        combinedScriptPrefix: 'nodepress-'
+    },
+    auth: {
+        cookieSecret: 'c00kie-key-4-hmac',
+        cookieName: '_npc'
+    },
+    view: {
+        viewRoot: path.join(__dirname, '/views'),
+        compress: true,
+        cache: true
+    }
+    ,env: 'development'
+    ,appRoot: __dirname
     ,host: '127.0.0.1'
     ,port: 8000
     ,installedApps: ['init', 'account', 'blog', 'management', 'static', 'tag']
+    ,middlewares: [
+        {name:'response-time'},
+        {name: 'error-handler'},
+        {name:'logger', level: 'info'},
+        {name:'conditional-get'},
+        {name: 'router', handler: 'genji.web.handler.SimpleCookieHandler'}
+    ]
 };
 
 try {
-    var local_settings = require('./settings');
+    var local_settings = require('./local_settings');
     for (var key in local_settings) {
         settings[key] = local_settings[key];
     }
-} catch(e) {}
+} catch(e) {
+    require('sys').puts('You can define settings in ./local_settings to override defaults.');
+}
 
 settings.libPath.forEach(function(path) {
     require.paths.unshift(path);
