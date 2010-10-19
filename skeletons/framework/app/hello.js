458ebc269a39eacfa3d3768aec9bf36c2e962a76
Senmiao Liu <zir.echo@gmail.com>
2010-10-19 23:28:21 +0800

    framework skeleton

--- skeletons/framework/app/hello.js
+++ skeletons/framework/app/hello.js
@@ -1,6 +1,17 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+var view = np.view;
 
+function helloworld() {
+    this.sendHTML('Hello world\n');
+}
 
+function notFound() {
+    var self = this;
+    view.render('404.html', {url: this.request.url}, function(html) {
+        self.error(404, html);
+    });
+}
+
+exports.view = [
+    ['^/hello/$', helloworld, 'get'],
+    ['.*', notFound, 'notfound']
+];
--- skeletons/framework/settings.js
+++ skeletons/framework/settings.js
@@ -1,6 +1,31 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+var path = require('path'),
+genji = require('genji');
+var settings = {
+    view: {
+        viewRoot: path.join(__dirname, '/view'),
+        compress: false,
+        cache: false
+    }
+    ,env: 'development'
+    ,appRoot: __dirname
+    ,host: '127.0.0.1'
+    ,port: 8000
+    ,installedApps: [{name: 'HelloWorld', require: path.join(__dirname, '/app/hello')}]
+    ,middlewares: [
+        {name:'response-time'},
+        {name: 'error-handler'},
+        {name:'logger', level: 'info'},
+        {name:'conditional-get'},
+        {name: 'router', handler: genji.web.handler.SimpleHandler}
+    ]
+};
+
+try {
+    var local_settings = require('./settings');
+    for (var key in local_settings) {
+        settings[key] = local_settings[key];
+    }
+} catch() {}
 
 
+module.exports = settings;
